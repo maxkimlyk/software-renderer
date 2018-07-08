d5898ec939e0987bc3fbe3952231ba8f831c8b0e
Max Kimlyk <maxskater3@yandex.ru>
2018-07-08 22:33:16 +0300

    Add Mat class

--- src/common/geometry.h
+++ src/common/geometry.h
@@ -116,4 +116,170 @@ typedef Vec<2, double> Vec2d;
 typedef Vec<3, double> Vec3d;
 typedef Vec<4, double> Vec4d;
 
+template<size_t n, class T>
+class Mat
+{
+    Vec<n, T> rows[n];
+
+public:
+    Vec<n, T>& operator[] (size_t row)
+    {
+        return rows[row];
+    }
+
+    const Vec<n, T>& operator[] (size_t row) const
+    {
+        return row[row];
+    }
+
+    Vec<n, T> Row(size_t i)
+    {
+        return rows[i];
+    }
+
+    Vec<n, T> Column(size_t j)
+    {
+        Vec<n, T> res;
+        for (size_t i = 0; i < n; ++i)
+            res[i] = rows[i][j];
+        return res;
+    }
+
+    Vec<n-1, T> Minor(size_t row, size_t col) // TODO: rename it. Minor is determinant of adjective matrix
+    {
+        Vec<n-1, T> minor;
+
+        size_t srcI = row == 0 ? 1 : 0;
+
+        for (int dstI = 0; dstI < n-1; ++dstI)
+        {
+            size_t srcJ = col == 0 ? 1 : 0;
+
+            for (int dstJ = 0; dstJ < n-1; ++dstJ)
+            {
+                minor[dstI][dstJ] = rows[srcI][srcJ];
+
+                if (++srcJ == col)
+                    ++srcJ;
+            }
+
+            if (++srcI == row)
+                ++srcI;
+        }
+
+        return minor;
+    }
+
+    T Determ()
+    {
+        return DetWrapper<n, T>::det(*this);
+    }
+};
+
+template <size_t n, class T>
+struct DetWrapper
+{
+    static inline T Sign(size_t index)
+    {
+        return 1 - 2 * (index & 1);
+    }
+
+    static T Det(Mat<n, T> &mat)
+    {
+        sum = 0;
+        for (int j = 0; j < n; ++j)
+        {
+            Mat<n-1, T> minor = mat.Minor(0, j);
+            T determ = DetWrapper<n-1, T>::Det(minor); // workaround for partial template function specialization
+            sum += Sign(0, j) * mat[0][j] * determ;
+        }
+        return sum;
+    }
+};
+
+template <class T>
+struct DetWrapper<1, T>
+{
+    static T Det(Mat<1, T> &mat)
+    {
+        return mat[0][0];
+    }
+};
+
+template<size_t n, class T>
+Mat<n, T> Transpose(Mat<n, T> &mat)
+{
+    Mat<n, T> res;
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < n; ++j)
+            res[i][j] = mat[j][i];
+    return res;
+}
+
+template<size_t n, class T>
+Mat<n, T> Inverse(Mat<n, T> &mat)
+{
+    Mat<n, T> res;
+    T det = mat.Determ();
+    T invDet = (T)(1) / det; // user should ensure that the matrix have inverse
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < n; ++j)
+        {
+            T minorDet = mat.Minor(i, j).Determ();
+            res[i][j] = minorDet * invDet;
+        }
+    return res;
+}
+
+template<size_t n, class T>
+Mat<n, T> operator* (Mat<n, T> &lhs, Mat<n, T> &rhs)
+{
+    Mat<n, T> res;
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < n; ++j)
+            res[i][j] = lhs[i] * rhs.Column(j);
+    return res;
+}
+
+template<size_t n, class T>
+Vec<n, T> operator* (Mat<n, T> &lhs, Vec<n, T> &rhs)
+{
+    Vec<n, T> res;
+    for (size_t i = 0; i < n; ++i)
+        res[i] = lhs[i] * rhs;
+}
+
+template<size_t n, class T>
+Vec<n, T> operator* (Mat<n, T> lhs, T rhs)
+{
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < n; ++j)
+            lhs[i][j] *= rhs;
+    return lhs;
+}
+
+template<size_t n, class T>
+Vec<n, T> operator* (T lhs, Mat<n, T> &rhs)
+{
+    return rhs * lhs;
+}
+
+template<size_t n, class T>
+Mat<n, T> operator+ (Mat<n, T> lhs, Mat<n, T> &rhs)
+{
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < n; ++j)
+            lhs[i][j] += rhs[i][j];
+    return lhs;
+}
+
+template<size_t n, class T>
+Mat<n, T> operator- (Mat<n, T> lhs, Mat<n, T> &rhs)
+{
+    for (size_t i = 0; i < n; ++i)
+        for (size_t j = 0; j < n; ++j)
+            lhs[i][j] -= rhs[i][j];
+    return lhs;
+}
+
 #endif
