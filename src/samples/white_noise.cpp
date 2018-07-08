034571461b1cb91134954d024d5a846f3a8012cb
Max Kimlyk <maxskater3@yandex.ru>
2018-07-08 17:11:04 +0300

    Structure: common - samples

--- src/samples/white_noise.cpp
+++ src/samples/white_noise.cpp
@@ -1,9 +1,8 @@
-#include "program.h"
-#include "renderer.h"
+#include "../common/program.h"
 
 const size_t WIDTH = 800;
 const size_t HEIGHT = 600;
-const std::string CAPTION = "TEST";
+const std::string CAPTION = "White Noise";
 
 void Process()
 {
