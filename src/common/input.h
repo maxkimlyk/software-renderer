5d901c583e633792f2e7cf0efd201d60804e33d6
Max Kimlyk <maxskater3@yandex.ru>
2020-06-20 16:58:02 +0300

    support click coordinates (linux)

--- src/common/input.h
+++ src/common/input.h
@@ -10,58 +10,75 @@ namespace sr
 
 class Input
 {
-    bool keys[256];
-    bool active;
-
   public:
+    struct Point
+    {
+        int x;
+        int y;
+    };
+
     Input()
     {
-        memset(keys, 0, sizeof(keys));
-        active = true;
+        memset(keys_, 0, sizeof(keys_));
+        active_ = true;
     }
 
     bool IsPressed(uint32_t key)
     {
-        bool state = keys[key];
-        keys[key] = 0;
+        bool state = keys_[key];
+        keys_[key] = 0;
         return state;
     }
 
-    bool IsHolding(uint32_t key)
+    bool IsHolding(uint32_t key) const
     {
-        return keys[key];
+        return keys_[key];
     }
 
     void OnKeyDown(uint32_t key)
     {
-        keys[key] = true;
+        keys_[key] = true;
     }
 
     void OnKeyUp(uint32_t key)
     {
-        keys[key] = false;
+        keys_[key] = false;
     }
 
     void OnFocus()
     {
-        active = true;
+        active_ = true;
     }
 
     void OnFocusLost()
     {
-        memset(keys, 0, sizeof(keys));
-        active = false;
+        memset(keys_, 0, sizeof(keys_));
+        active_ = false;
     }
 
-    void OnMouseButtonDown(uint32_t button, int /* x */, int /* y */) {
-        keys[button] = true;
-
+    void OnMouseButtonDown(uint32_t button, int x, int y)
+    {
+        keys_[button] = true;
+        click_points_[button] = Point{x, y};
     }
 
-    void OnMouseButtonUp(uint32_t button, int /* x */, int /* y */) {
-        keys[button] = false;
-
+    void OnMouseButtonUp(uint32_t button, int /* x */, int /* y */)
+    {
+        keys_[button] = false;
     }
+
+    Point GetClickPoint(uint32_t button = KEY_LBUTTON) const
+    {
+        return click_points_[button];
+    }
+
+  private:
+    static const size_t MAX_MOUSE_BUTTON_KEYCODE = static_cast<size_t>(KEY_XBUTTON2);
+
+    bool keys_[256];
+    bool active_;
+
+    Point click_points_[MAX_MOUSE_BUTTON_KEYCODE + 1];
 };
 
 } // namespace sr
